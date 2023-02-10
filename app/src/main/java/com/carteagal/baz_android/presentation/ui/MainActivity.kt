f7b4291b21077de6057460f888ed17d971031fc6
charlye917R <carlos.arteaga@raven.inc>
2023-02-10 16:43:29 -0600

    screen error

--- app/src/main/java/com/carteagal/baz_android/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/carteagal/baz_android/presentation/ui/MainActivity.kt
@@ -21,8 +21,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
     private lateinit var checkNetworkConnection: CheckInternetConnection
-    private val cryptoViewModel: CryptoViewModel by viewModels()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
